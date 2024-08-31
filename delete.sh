ebdca78cfecd2077a4dcf1d6232af5f7c081b37a
macnaer <trofimchuk.an@gmail.com>
2024-08-31 10:58:02 +0300

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -3,7 +3,7 @@ docker kill $(docker ps -q) 2> /dev/null
 if [[ $! == 0 ]];then
         docker kill $(docker ps -q)
 fi
-sudo docker rm $(docker ps -aq) 2> /dev/null
+docker rm $(docker ps -aq) 2> /dev/null
 if [[ $! == 0 ]];then
         docker rm $(docker ps -aq)
 fi
