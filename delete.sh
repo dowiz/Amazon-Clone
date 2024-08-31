4f2095ed0ad2fe43a98f31378936caa300eb2c3c
macnaer <trofimchuk.an@gmail.com>
2024-08-31 10:05:58 +0300

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-docker kill $(docker ps -q) 2> /dev/null
+sudo docker kill $(docker ps -q) 2> /dev/null
 if [[$! == 0]];then
-        docker kill $(docker ps -q)
+        sudo docker kill $(docker ps -q)
 fi
-docker rm $(docker ps -aq) 2> /dev/null
+sudo docker rm $(docker ps -aq) 2> /dev/null
 if [[$! == 0]];then
-        docker rm $(docker ps -aq)
+        sudo docker rm $(docker ps -aq)
 fi
-yes | docker image prune -a
+yes | sudo docker image prune -a
