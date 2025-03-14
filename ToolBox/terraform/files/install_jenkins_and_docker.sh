564d541e1ee2cea6cdb52ec70b82d5d960c70e64
dowiz <eriomin.oe@gmail.com>
2025-03-14 21:58:27 +0200

    save changes

---
+++ ToolBox/terraform/files/install_jenkins_and_docker.sh
@@ -0,0 +1,128 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install -y wget gnupg2 software-properties-common curl jq
+
+# sudo add-apt-repository -y ppa:openjdk-r/ppa
+sudo apt update
+
+latest_lts_java_version=$(curl -s https://api.adoptium.net/v3/info/available_releases | jq -r '.most_recent_lts')
+echo "Актуальна версія Java LTS: $latest_lts_java_version"
+
+sudo apt install -y openjdk-$latest_lts_java_version-jdk
+
+jenkins_key_page=$(curl -s https://pkg.jenkins.io/debian-stable/)
+jenkins_key_url=$(echo "$jenkins_key_page" | grep -oP 'https://[^"]+jenkins.io-\d{4}.key' | head -1)
+
+curl -fsSL $jenkins_key_url | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+
+sudo apt-get -y update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable docker
+
+sudo usermod -aG docker jenkins
+sudo usermod -aG adm jenkins
+sudo systemctl  restart jenkins
+
+
+sudo mkdir /opt/mssql
+sudo chmod 777 /opt/mssql/
+
+###################Unlock Jenkins###########################
+url="http://localhost:8080"
+password=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+
+echo $password
+
+# NEW ADMIN CREDENTIALS URL ENCODED USING BASH
+username="master"
+new_password="Qwerty-1"
+fullname="master"
+email="example@gmail.com"
+
+cookie_jar="$(mktemp)"
+full_crumb=$(curl -u "admin:$password" --cookie-jar "$cookie_jar" $url/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+arr_crumb=(${full_crumb//:/ })
+only_crumb=$(echo ${arr_crumb[1]})
+
+
+# MAKE THE REQUEST TO CREATE AN ADMIN USER
+curl -X POST -u "admin:$password" $url/setupWizard/createAdminUser \
+        -H "Connection: keep-alive" \
+        -H "Accept: application/json, text/javascript" \
+        -H "X-Requested-With: XMLHttpRequest" \
+        -H "$full_crumb" \
+        -H "Content-Type: application/x-www-form-urlencoded" \
+        --cookie $cookie_jar \
+        --data-raw "username=$username&password1=$new_password&password2=$new_password&fullname=$fullname&email=$email&Jenkins-Crumb=$only_crumb&json=%7B%22username%22%3A%20%22$username%22%2C%20%22password1%22%3A%20%22$new_password%22%2C%20%22%24redact%22%3A%20%5B%22password1%22%2C%20%22password2%22%5D%2C%20%22password2%22%3A%20%22$new_password%22%2C%20%22fullname%22%3A%20%22$fullname%22%2C%20%22email%22%3A%20%22$email%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D&core%3Aapply=&Submit=Save&json=%7B%22username%22%3A%20%22$username%22%2C%20%22password1%22%3A%20%22$new_password%22%2C%20%22%24redact%22%3A%20%5B%22password1%22%2C%20%22password2%22%5D%2C%20%22password2%22%3A%20%22$new_password%22%2C%20%22fullname%22%3A%20%22$fullname%22%2C%20%22email%22%3A%20%22$email%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D"
+#################################################
+################################# Suggested Plugins ############################################
+#! /bin/bash
+url=http://localhost:8080
+
+
+cookie_jar="$(mktemp)"
+full_crumb=$(curl -u "$username:$new_password" --cookie-jar "$cookie_jar" $url/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+arr_crumb=(${full_crumb//:/ })
+only_crumb=$(echo ${arr_crumb[1]})
+
+# MAKE THE REQUEST TO DOWNLOAD AND INSTALL REQUIRED MODULES
+curl -X POST -u "$username:$new_password" $url/pluginManager/installPlugins \
+  -H 'Connection: keep-alive' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H "$full_crumb" \
+  -H 'Content-Type: application/json' \
+  -H 'Accept-Language: en,en-US;q=0.9,it;q=0.8' \
+  --cookie $cookie_jar \
+  --data-raw "{'dynamicLoad':true,'plugins':['cloudbees-folder','antisamy-markup-formatter','build-timeout','credentials-binding','timestamper','ws-cleanup','ant','gradle','workflow-aggregator','github-branch-source','pipeline-github-lib','pipeline-stage-view','git','ssh-slaves','matrix-auth','pam-auth','ldap','email-ext','mailer'],'Jenkins-Crumb':'$only_crumb'}"
+###################################################################################
+
+############################################### Confirm URL ###################################################
+
+#! /bin/bash
+
+# The jenkins URL, if you are on an amazon instance, you can put there the
+# public DNS name, in order to be able to access jenkins thorugh that URL
+url=http://localhost:8080
+
+user=user
+password=password
+url_urlEncoded=$(python -c "import urllib;print urllib.quote(raw_input(), safe='')" <<< "$url")
+
+cookie_jar="$(mktemp)"
+full_crumb=$(curl -u "$username:$new_password" --cookie-jar "$cookie_jar" $url/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
+arr_crumb=(${full_crumb//:/ })
+only_crumb=$(echo ${arr_crumb[1]})
+
+curl -X POST -u "$username:$new_password" $url/setupWizard/configureInstance \
+  -H 'Connection: keep-alive' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H "$full_crumb" \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Accept-Language: en,en-US;q=0.9,it;q=0.8' \
+  --cookie $cookie_jar \
+  --data-raw "rootUrl=$url_urlEncoded%2F&Jenkins-Crumb=$only_crumb&json=%7B%22rootUrl%22%3A%20%22$url_urlEncoded%2F%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D&core%3Aapply=&Submit=Save&json=%7B%22rootUrl%22%3A%20%22$url_urlEncoded%2F%22%2C%20%22Jenkins-Crumb%22%3A%20%22$only_crumb%22%7D"
+############################################################################################
+
+sudo systemctl restart jenkins
